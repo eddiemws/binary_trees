a79e9510788646a750d07ca3733101999d92793c
eddy <106661971+eddiemws@users.noreply.github.com>
2024-06-07 17:07:48 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_nod->n = value;
 	new_nod->parent = parent;
 	new_nod->left = NULL;
-	new_nod->right = NULL;
+	
 
 	return (new_nod);
 }
