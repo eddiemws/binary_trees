0f5e2687c30ce4153d18e7d797bf3d581b75b6c4
eddy <106661971+eddiemws@users.noreply.github.com>
2024-06-13 19:14:16 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,4 +24,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * Return: If tree is NULL, return 0, else return balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
 
+    int left_height = (tree->left != NULL) ? (int)binary_tree_height(tree->left) : 0;
+    int right_height = (tree->right != NULL) ? (int)binary_tree_height(tree->right) : 0;
+
+    return (left_height - right_height);
+}
