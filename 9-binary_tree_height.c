cf798a29339919e87b6cdd7fc20fe0352cdf923b
eddy <106661971+eddiemws@users.noreply.github.com>
2024-06-13 19:16:46 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	
+	if (tree)
+	{
+		size_t lef = 0, rig = 0;
+
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((lef > rig) ? lef : rig);
+	}
 
 	return (0);
 }
