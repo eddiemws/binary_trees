a00c8724e7c8e920ca37a5a3d1a841fc3c1f4598
eddy <106661971+eddiemws@users.noreply.github.com>
2024-10-25 22:17:27 +0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,3 +6,13 @@
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
+
