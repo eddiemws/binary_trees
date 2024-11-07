73245685baf4b78df2d2f36ee57818493a7ce44d
eddy <106661971+eddiemws@users.noreply.github.com>
2024-11-07 22:23:58 +0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,7 +11,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->right, func);
+		
 		func(tree->n);
 	}
 }
